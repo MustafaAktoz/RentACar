b11e479149dfcb74d8f5535452c0c882c2bd8806
MustafaAktoz <87762766+MustafaAktoz@users.noreply.github.com>
2021-12-11 20:42:49 +0300

    11.12.2021 20:42

--- Business/ValidationRules/FluentValidation/CarValidator.cs
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -13,7 +13,7 @@ namespace Business.ValidationRules.FluentValidation
         {
             RuleFor(c => c.Name).MinimumLength(2);
             RuleFor(c => c.DailyPrice).GreaterThan(0);
-            RuleFor(c => c.DailyPrice).GreaterThanOrEqualTo(400).When(c => c.BrandId == 1).WithMessage(Messages.LowPriceForThisBrand);
+            RuleFor(c => c.DailyPrice).GreaterThanOrEqualTo(200).When(c => c.BrandId == 1).WithMessage(Messages.LowPriceForThisBrand);
             RuleFor(c => c.FindeksPoint).GreaterThanOrEqualTo(0);
             RuleFor(c => c.FindeksPoint).LessThanOrEqualTo(1900);
         }
